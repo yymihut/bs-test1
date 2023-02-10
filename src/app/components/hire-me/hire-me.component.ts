3e64945814c8bf11decb8ea6cfb9ac5fa5286e48
yymihute <yymihut@gmail.comm>
2023-02-10 16:18:56 +0200

    kbkbkj

--- src/app/components/hire-me/hire-me.component.ts
+++ src/app/components/hire-me/hire-me.component.ts
@@ -8,7 +8,6 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs/internal/Observable';
 import { ConfirmationModalComponent } from 'src/app/shared/confirmation-modal/confirmation-modal.component';
 
 @Component({
@@ -31,60 +30,23 @@ export class HireMeComponent implements OnInit {
 
   sex = ['femeie', 'barbat'];
   dateForm!: FormGroup;
-  numeInterzise: string[] = ['Ioan', 'Maria']; // le folosim sa creeam un validator custom
 
   ngOnInit(): void {
     this.dateForm = new FormGroup({
-      nume: new FormControl(null, [
-        Validators.required,
-        this.Interzise.bind(this),
-      ]),
       subiect: new FormControl(null, Validators.required),
-      email: new FormControl(
-        null,
-        [Validators.required, Validators.email],
-        this.emailInterzis
-      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       sex: new FormControl('male'),
       mesaj: new FormControl(null, Validators.required),
       data: new FormControl(null, Validators.required),
       hobbyes: new FormArray([]),
     });
-    // valuechanges and status - aflam date despre form in timp real
-    // this.dateForm.valueChanges.subscribe((value) => console.log(value));
-    // this.dateForm.statusChanges.subscribe((status) => console.log(status));
-  }
-  get arrayHobyes() {
-    return <FormArray>this.dateForm.get('hobbyes');
   }
   onSubmit() {
     console.log(this.dateForm);
-    this.dateForm.reset();
   }
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    (<FormArray>this.dateForm.get('hobbyes')).push(control);
-  }
-
-  // un validator pt numele interzise din array,  este o functie:
-  Interzise(control: FormControl) {
-    if (this.numeInterzise.indexOf(control.value) !== -1) {
-      return { 'numele-este-interzis': true }; //eroarea care se trece la obiectul erori
-    }
-    return null;
-  }
-  //atunci cand aducem de la server, trebuie async promise validator
-  emailInterzis(control: FormControl) {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@test.com') {
-          resolve({ 'emailul-este-interzis': true });
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-    return promise;
+    (<FormArray>this.dateForm.get('hobbies')).push(control);
   }
 
   // varianta de form cu template
